e0170c2116551a5dadc95fb47ac657a328085868
jameshu <513826883@qq.com>
2016-07-27 17:21:05 +0800

    首次提交到本地仓库

---
+++ weixincheck/src/org/jameshu/course/util/CoreServlet.java
@@ -0,0 +1,33 @@
+package org.jameshu.course.util;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CoreServlet extends HttpServlet {
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+		String signature = request.getParameter("signature");
+		// 时间戳
+		String timestamp = request.getParameter("timestamp");
+		// 随机数
+		String nonce = request.getParameter("nonce");
+		// 随机字符串
+		String echostr = request.getParameter("echostr");
+
+		PrintWriter out = response.getWriter();
+		// 通过检验signature对请求进行校验，若校验成功则原样返回echostr，表示接入成功，否则接入失败
+		if (SignUtil.checkSignature(signature, timestamp, nonce)) {
+			out.print(echostr);
+			System.out.println(echostr);
+		} else {
+			System.out.println("error");
+		}
+		out.close();
+		out = null;
+	}
+}
